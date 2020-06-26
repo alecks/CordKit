ba3f1e72f38d3f05125aa0426e67b5811969aad7
Alex P <alexeek@protonmail.com>
2020-06-26 12:01:50 +0100

    refactor: Remove unused todo

--- src/mod.ts
+++ src/mod.ts
@@ -3,7 +3,6 @@ import Command from './command';
 
 export default class CordKit {
 	client = new eris.Client(this.token)
-	// TODO: Use Command type.
 	commands = new Map<string, Command>();
 
 	constructor(private token: string) {}
