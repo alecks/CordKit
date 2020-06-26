ab4237329ab68ef0d687b42223b6ffa193fc30eb
Alex P <alexeek@protonmail.com>
2020-06-26 12:14:17 +0100

    docs: Document the CordKit class

--- src/mod.ts
+++ src/mod.ts
@@ -1,9 +1,18 @@
 import * as eris from 'eris'
 import Command from './command'
 
+/**
+ * CordKit is the main tester.
+ * It has an Eris Discord client and contains the commands Map; you should add to this.
+ */
 export default class CordKit {
 	client = new eris.Client(this.token)
 	commands = new Map<string, Command>()
 
+	/**
+	 * Constructs CordKit.
+	 * @param token The tester's bot token.
+	 * @param botID The ID of the bot to test.
+	 */
 	constructor(private token: string, public botID: string) {}
 }
