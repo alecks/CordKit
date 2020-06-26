ee8105902497a6c0185c8c130bed0b675a41ffee
Alex P <alexeek@protonmail.com>
2020-06-26 10:26:00 +0100

    start TypeScript rewrite

--- go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/fjah/CordKit
-
-go 1.14
-
-require (
-	github.com/andersfylling/disgord v0.18.0
-)
--- tester_test.go
+++
@@ -1,7 +0,0 @@
-package cordkit
-
-import "testing"
-
-func TestTester(t *testing.T) {
-	t.Fatal("unimplemented")
-}
