81cde8744dc7de1a8fc4965d5f4dd1ac5c2b5b30
Alex P <alexeek@protonmail.com>
2020-06-26 11:28:21 +0100

    feat: Add commands map

--- src/mod.ts
+++ src/mod.ts
@@ -2,6 +2,8 @@ import * as eris from 'eris'
 
 export default class CordKit {
 	client = new eris.Client(this.token)
+	// TODO: Use Command type.
+	commands = new Map<string, undefined>();
 
 	constructor(private token: string) {}
 }
